ca68c3dcb2bc08bb1f23cf6f9343ef0933d3b671
Duy khiem <duykhiemhaui@gmail.com>
2021-06-16 12:03:05 +0700

    update code

---
+++ src/store/ListSearch/functions.ts
@@ -0,0 +1,93 @@
+import { Fetch } from "@/utils/fetch";
+import { syncAllCategories } from "@/store/categories";
+import { RawCategory, RawFriend, RawPost } from "@/types";
+import { syncAllPosts } from "@/store/post/index";
+import { getPreviewData } from "@/utils/preview";
+import { syncAllFiends } from "@/store/Friend/index";
+
+
+const fakeData: RawFriend[] = [
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://hinhanhdep.org/wp-content/uploads/2016/07/anh-avatar-girl-xinh.jpg",
+    name: "Định Pu"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://photographer.com.vn/wp-content/uploads/2020/08/1598380289_Hinh-nen-Girl-Xinh-tit-mu-tap-cho-desktop-laptop.jpg",
+    name: "Duy khiêm"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://wikihow.com.vn/wp-content/uploads/2020/07/anh-gai-xinh-trung-hoc-co-so.jpg",
+    name: "Hà Quang"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://th.bing.com/th/id/R859ee27e113224cb26bab1a5d69a1967?rik=Cwm5sJmsxxKNHg&pid=ImgRaw",
+    name: "Sơn Tùng"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://hinhanhdep.org/wp-content/uploads/2016/07/anh-avatar-girl-xinh.jpg",
+    name: "Định Pu"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://hinhanhdep.org/wp-content/uploads/2016/07/anh-avatar-girl-xinh.jpg",
+    name: "Định Pu"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://thuthuatnhanh.com/wp-content/uploads/2019/07/anh-girl-xinh-facebook-tuyet-dep-387x580.jpg",
+    name: "Định Pu"
+
+  },
+  {
+    id: "",
+    title: "50 bạn chung",
+    avatar: "https://hinhanhdep.org/wp-content/uploads/2016/07/anh-avatar-girl-xinh.jpg",
+    name: "Hiền Hồ"
+
+  }
+
+];
+export const requestGetFriendList = async () => {
+  // const {data} = await Fetch.get<{ result: RawCategory[] }>('category/list', {
+  //     params: {
+  //         limit: 99999,
+  //         level: 0,
+  //         active: 1,
+  //         page: 1,
+  //     }
+  // });
+  // if (!data || !data?.result || !data?.result) {
+  //     return []
+  // }
+  let result: RawFriend[] = [];
+
+  for (let i = 0; i < 50; i++) {
+    const _index = Math.floor(Math.random() * fakeData.length);
+    result.push({
+      ...fakeData[_index],
+      id: i.toString()
+    });
+  }
+
+  syncAllFiends(result);
+  return result.map(item => item.id);
+};
+
---
+++ src/store/ListSearch/index.ts
@@ -0,0 +1,36 @@
+import {createDynamicReducer} from '@/utils/createDynamicReducer';
+import {RawFriend} from '@/types';
+import {batch, useSelector} from 'react-redux';
+
+const {
+  setStore,
+  reducer,
+  sync,
+  useByKey,
+  setQueries,
+  useKeysByQuery,
+} = createDynamicReducer<RawFriend>('friends', 'id');
+
+export const setFriendStore = setStore;
+export const friendReducer = reducer;
+export const syncFriends = sync;
+export const useFriend = useByKey;
+export const setFriendQueries = setQueries;
+export const useFriendsByQuery = useKeysByQuery;
+
+export const syncAllFiends = (accessories: RawFriend[]) => {
+  let query: { [id: string]: string[] } = {};
+  let ids: string[] = [];
+
+  for (let access of accessories) {
+    ids.push(access.id.toString());
+  }
+
+  batch(() => {
+    syncFriends(accessories);
+    setFriendQueries({
+      all: ids,
+      ...query,
+    });
+  });
+};
